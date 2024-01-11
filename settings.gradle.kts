95049dac61c2e71580d3110898df95e051c77bc8
“Oliver <oli.kern@hotmail.com>
2024-01-11 09:53:23 +0100

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "oamk_mobile_native"
+include(":app")
